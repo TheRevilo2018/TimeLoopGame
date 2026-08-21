9f4de10b96ff1c6fe007def03c2c29cd64efc540
TheRevilo2018 <ojuhl2000@gmail.com>
2026-08-21 15:43:26 -0600

    Made input recording

--- TimeLoopGame/Assets/Entities/Character/playerMovement.cs
+++ TimeLoopGame/Assets/Entities/Character/playerMovement.cs
@@ -1,34 +1,76 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
+    const float FORCE_MULT = 10;
+
     public Rigidbody rb;
-    public InputAction input;
+    public InputAction movement;
+    public InputAction toggleRecord;
+
+    private Vector2 currentForce = new Vector2(0, 0);
+    private bool recording = false;
+    private List<Vector3> inputs = new List<Vector3>();
+    private int inputIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        toggleRecord.performed += ToggleRecord_performed;
     }
 
     private void OnEnable()
     {
-        input.Enable();
+        movement.Enable();
+        toggleRecord.Enable();
     }
 
     private void OnDisable()
     {
-        input.Disable();
+        movement.Disable();
+        toggleRecord.Disable();
+        recording = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        var speed = input.ReadValue<Vector2>();
+        Vector3 newForce = new Vector3();
+        if (recording)
+        {
+            newForce = new Vector3(currentForce.x * FORCE_MULT, 0, currentForce.y * FORCE_MULT);
+            inputs.Add(newForce);
+        }
+        else
+        {
+            inputIndex++;
+            if (inputIndex >= inputs.Count) inputIndex = 0;
 
-        rb.AddForce(new Vector3(speed.x, 0, speed.y));
+            newForce = inputs[inputIndex];
+        }
+
+        rb.AddForce(newForce);
+    }
+
+    private void Update()
+    {
+        currentForce = movement.ReadValue<Vector2>();
+    }
+
+    private void ToggleRecord_performed(InputAction.CallbackContext obj)
+    {
+        toggleRecording();
+    }
+
+    private void toggleRecording()
+    {
+        if (!recording)
+        {
+            inputs.Clear();
+            inputIndex = 0;
+        }
+        recording = !recording;
     }
 }
-   
